0fca2eb2512298dc8339ca257b7e9f0c22f77862
Mazyviy <maxsonyxperia25@mail.ru>
2021-05-08 22:39:21 +0400

    update

--- reseach_operation/reseach_operation.cpp
+++ reseach_operation/reseach_operation.cpp
@@ -1,5 +1,4 @@
 ﻿// reseach_operation.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
 #define _USE_MATH_DEFINES
 #include <iostream>
 #include <math.h>
@@ -37,7 +36,7 @@ int main(){
 		}
 	}
 
-	array_sum[0][col - 1] = array[0][col - 1];
+	array_sum[0][col-1] = array[0][col-1];
 
 	for(j=col-2; j>=0; --j){
 		array_sum[0][j]=array_sum[0][j+1]+array[0][j];
@@ -54,68 +53,72 @@ int main(){
 		}
 	}
 
-	cout << "Array" << endl;
-	for (i = 0; i < row; ++i) {
-		for (j = 0; j < col; ++j) {
-			cout << fixed << setw(10) << setprecision(8) << array[i][j] << " ";
+	cout<<"Array"<<endl;
+	for (i=0; i<row; ++i){
+		for (j=0; j<col; ++j){
+			cout<<fixed<<setw(10)<<setprecision(8)<<array[i][j]<< " ";
 		}
-		cout << endl;
+		cout<<endl;
 	}
-	cout << endl;
-	cout <<"Array_sum" <<endl;
-	for (i = 0; i < row; ++i) {
-		for (j = 0; j < col; ++j) {
-			cout << fixed << setw(10) << setprecision(8) << array_sum[i][j] << " ";
+	cout<<endl;
+	cout<<"Array_sum"<<endl;
+	for (i=0; i<row; ++i){
+		for (j=0; j<col; ++j){
+			cout<<fixed<<setw(10)<<setprecision(8)<<array_sum[i][j]<< " ";
 		}
-		cout << endl;
+		cout<<endl;
 	}
-	cout << endl;
+	cout<<endl;
 
-	j = 0;
-	i = row-1;
+	j=0;
+	i=row-1;
 
-	cout << "Way: " << array_sum[row - 1][0] << endl;
-	cout <<"(" << i << "," << j << ")";
-	while (true) {
-		if (i == 0 && j == col - 1) {
+	cout<<"Way: "<<array_sum[row-1][0]<<endl;
+	cout<<"("<<i<<","<<j<<")";
+	while(true){
+		if(i==0 && j==col-1){
 			break;
 		}
-		if (j != col-1) {
-			if (i != 0 && (array_sum[i - 1][j] <= array_sum[i][j + 1])) {
-				cout << "-(" << i - 1 << "," << j << ")";
-				array_path[i - 1][j] = 1;
+		if(j!=col-1){
+			if(i!=0 && (array_sum[i-1][j]<=array_sum[i][j+1])){
+				cout<<"-("<<i-1<<","<<j<<")";
+				array_path[i-1][j]=1;
 				i--;
 			}
-			else {
-				cout << "-(" << i << "," << j + 1 << ")";
-				array_path[i][j+1] = 1;
+			else{
+				cout<<"-("<<i<<","<< j+1<<")";
+				array_path[i][j+1]=1;
 				j++;
 			}
 		}
-		else {
-			cout << "-(" << i - 1 << "," << j << ")";
-			array_path[i - 1][j] = 1;
+		else{
+			cout<<"-("<<i-1<<","<<j<< ")";
+			array_path[i-1][j]=1;
 			j--;
 		}
 	}
-	cout << endl;
-	cout << endl;
+	cout<<endl;
+	cout<<endl;
 
-	array_path[row - 1][0] = 1;
+	array_path[row-1][0]=1;
 
-	for (i = 0; i < row; ++i) {
-		for (j = 0; j < col; ++j) {
-			if (array_path[i][j] == 1)
-				cout << text_style_color_start << array[i][j]<< text_style_end<<" ";
+	for(i=0; i<row; ++i){
+		for(j=0; j<col; ++j){
+			if(array_path[i][j]==1)
+				cout<<text_style_color_start<<array[i][j]<<text_style_end<<" ";
 			else
-				cout << array[i][j]<< " ";
+				cout<<array[i][j]<<" ";
 		}
-		cout << endl;
+		cout<<endl;
 	}
 
 	for(i=0; i<row; i++){
-		delete[] array[i], array_sum[i], array_path[i];
+		delete[] array[i];
+		delete[] array_sum[i];
+		delete[] array_path[i];
 	}
-	delete[] array, array_sum, array_path;
+	delete[] array;
+	delete[] array_sum;
+	delete[] array_path;
 	return 0;
 }
